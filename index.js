bedcd3bb5d4ab598f332ed80536374c4d0c61e9f
Tarangok <tarangok@mail.ru>
2019-10-29 23:55:06 +0700

    Add code for working

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const electron = require('electron');
+var io = require('socket.io').listen(8080); 
+
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
@@ -14,7 +16,31 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 400, height: 600});
   mainWindow.loadURL('file://' + __dirname + '/web/index.html');
-
+  
+  io.set('log level', 1);
+  // Навешиваем обработчик на подключение нового клиента
+  io.sockets.on('connection', function (socket) {
+    // Т.к. чат простой - в качестве ников пока используем первые 5 символов от ID сокета
+    var ID = (socket.id).toString().substr(0, 5);
+    var time = (new Date).toLocaleTimeString();
+    // Посылаем клиенту сообщение о том, что он успешно подключился и его имя
+    socket.json.send({'event': 'connected', 'name': ID, 'time': time});
+    // Посылаем всем остальным пользователям, что подключился новый клиент и его имя
+    socket.broadcast.json.send({'event': 'userJoined', 'name': ID, 'time': time});
+    // Навешиваем обработчик на входящее сообщение
+    socket.on('message', function (msg) {
+      var time = (new Date).toLocaleTimeString();
+      // Уведомляем клиента, что его сообщение успешно дошло до сервера
+      socket.json.send({'event': 'messageSent', 'name': ID, 'text': msg, 'time': time});
+      // Отсылаем сообщение остальным участникам чата
+      socket.broadcast.json.send({'event': 'messageReceived', 'name': ID, 'text': msg, 'time': time})
+    });
+    // При отключении клиента - уведомляем остальных
+    socket.on('disconnect', function() {
+      var time = (new Date).toLocaleTimeString();
+      io.sockets.json.send({'event': 'userSplit', 'name': ID, 'time': time});
+    });
+  }); 
   
   mainWindow.on('closed', function() {
     mainWindow = null;
