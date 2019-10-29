26c92079a0675883e8466ae0520240fa05ee809f
Tarangok <tarangok@mail.ru>
2019-10-29 23:43:03 +0700

    Add base functional

---
+++ index.js
@@ -0,0 +1,22 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 400, height: 600});
+  mainWindow.loadURL('file://' + __dirname + '/web/index.html');
+
+  
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
